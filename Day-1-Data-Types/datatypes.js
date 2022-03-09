91c9e7e74e7a5c713c14db90ba38474cff0f18ff
İlknur Nurioğlu <97106063+ilknur-nurioglu@users.noreply.github.com>
2022-03-09 12:00:50 +0300

    datatypes.js

---
+++ Day-1-Data-Types/datatypes.js
@@ -0,0 +1,13 @@
+    // Declare second integer, double, and String variables.
+    var myInt, myDouble, myStr
+    // Read and save an integer, double, and String to your variables.
+    myInt = parseInt(readLine());
+    myDouble = parseFloat(readLine());
+    myStr = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(i+myInt)
+    // Print the sum of the double variables on a new line.
+    console.log((d+myDouble).toFixed(1))
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+myStr)
