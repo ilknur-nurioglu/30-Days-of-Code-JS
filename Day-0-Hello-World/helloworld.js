7b68189750f1be20e5d433ebac5c772840619dcd
İlknur Nurioğlu <97106063+ilknur-nurioglu@users.noreply.github.com>
2022-03-09 11:28:20 +0300

    helloworld.js

--- Day-0-Hello-World/helloworld.js
+++ Day-0-Hello-World/helloworld.js
@@ -3,6 +3,5 @@ function processData(inputString) {
     console.log("Hello, World.");
     
     // Write the second line of output that prints the contents of 'inputString' here.
-    inputString = "Welcome to 30 Days of Code!"
     console.log(inputString)
 }
